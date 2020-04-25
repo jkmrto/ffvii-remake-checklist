e9ee67ce0a3643f696ffe63382a3def0d76a8046
jkmrto <juanka1331@gmail.com>
2020-04-25 19:32:02 +0200

    missing files

---
+++ app/Colors.ts
@@ -0,0 +1,4 @@
+export let blue = {
+  light: 'rgb(165,180,193)', //Background
+  ultraLight: 'rgb(240,248,255)',
+};
---
+++ app/Domain.ts
@@ -0,0 +1,6 @@
+export type SideQuest = {
+  title: string;
+  checked: boolean;
+  chapter: number;
+  index: number;
+};
