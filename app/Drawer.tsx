64593569970941d5a870b369d378428c1d9e2311
jkmrto <juanka1331@gmail.com>
2020-05-05 23:33:41 +0200

    wip

---
+++ app/Drawer.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import {
+  createDrawerNavigator,
+  DrawerItems,
+  DrawerContentComponentProps,
+} from 'react-navigation-drawer';
+
+import CircularProgress from './components/CircularProgress';
+
+// Screens
+import SideQuestsScreen from './screens/SideQuestsScreen';
+import WeaponsScreen from './screens/WeaponsScreen';
+
+import * as GlobalContext from './GlobalContext';
+
+const CustomDrawerContentComponent = (props: DrawerContentComponentProps) => {
+  const theme = GlobalContext.useTheme();
+
+  return (
+    <ScrollView>
+      <SafeAreaView style={{flex: 1}}>
+        <DrawerItems {...props} />
+        <CircularProgress percentage={theme.isOnline}>
+          <View>
+            <Text>{theme.isOnline}%</Text>
+          </View>
+        </CircularProgress>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const Drawer = createDrawerNavigator(
+  {
+    'Side Quests': {
+      screen: SideQuestsScreen,
+    },
+    Weapons: {
+      screen: WeaponsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Side Quests',
+    contentComponent: CustomDrawerContentComponent,
+    contentOptions: {
+      activeTintColor: '#000000',
+      activeBackgroundColor: '#e6e6e6',
+    },
+  },
+);
+
+export default Drawer;
--- index.js
+++ index.js
@@ -5,5 +5,6 @@ if (__DEV__) {
 import {AppRegistry} from 'react-native';
 import App from './app/App.tsx';
 import {name as appName} from './app.json';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
